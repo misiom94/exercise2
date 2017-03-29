2ede962312ae64405cf81f99a2f18b809a79c35a
Mikla <michal.mikla@accenture.com>
2017-03-29 18:10:25 +0200

    procon homework

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,34 @@
 package wdsr.exercise2.procon;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	private List<Order> orders = new LinkedList<>();
+	private Semaphore s_semaphore = new Semaphore(0);
+	private Semaphore s_mut = new Semaphore(1);
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		s_mut.acquire();
+		orders.add(order);
+		s_mut.release();
+		s_semaphore.release();
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		Order order = null;
+		s_semaphore.acquire(1);
+		s_mut.acquire();
+		order = orders.remove(0);
+		s_mut.release();
+		return order;
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,18 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+	private BlockingQueue<Order> blocking_queue = new LinkedBlockingQueue<Order>();
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		blocking_queue.put(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		return blocking_queue.take();
 	}
 }
